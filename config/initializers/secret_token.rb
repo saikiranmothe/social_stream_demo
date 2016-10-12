7b821c73a94c3045c179bfa41fc961aadfbb69c5
saikiranmothe <saikiran.mothe@gmail.com>
2016-10-11 21:01:40 -0400

    make it better

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socialstream::Application.config.secret_token = '061a1f0959cfd0b1df384a88fe280bbf2ecdd38b4151d5f14835c070c1a34fda9f93080a75a0895db35579747ee5447bb0369afa797b1a559019e97174336be0'
